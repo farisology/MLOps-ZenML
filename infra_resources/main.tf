d8c430250e91058c20882f3fcce6b14636aec2b4
Fares Hasan <farishassan2012@gmail.com>
2023-05-21 11:51:48 +0800

    adding airflow resources

--- infra_resources/main.tf
+++ infra_resources/main.tf
@@ -42,11 +42,11 @@ module "dev_ssh_sg" {
   }
 }
 
-module "zenml_ec2_sg" {
+module "default_ec2_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "zenml_ec2_sg"
-  description = "Security group for zenml_ec2_sg"
+  name        = "default_ec2_sg"
+  description = "Security group for deafult access"
   vpc_id      = data.aws_vpc.default.id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -54,7 +54,7 @@ module "zenml_ec2_sg" {
   egress_rules        = ["all-all"]
 
   tags = {
-    Name    = "zenml_ec2_sg"
+    Name    = "default_ec2_sg"
     Project = "MLOps"
     Owner   = "Farisology"
 
@@ -70,7 +70,7 @@ resource "aws_instance" "zenml_server" {
     volume_size = 30
   }
   vpc_security_group_ids = [
-    module.zenml_ec2_sg.security_group_id,
+    module.default_ec2_sg.security_group_id,
     module.dev_ssh_sg.security_group_id,
   ]
   tags = {
@@ -102,4 +102,67 @@ resource "aws_ecr_repository" "zenml_registry" {
     Project = "MLOps"
     Owner   = "Farisology"
   }
+}
+
+resource "aws_security_group" "airflow_sg" {
+  name        = "airflow_rules"
+  description = "Allow inbound traffic to webserver, flower potsgres and redis"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5555
+    to_port     = 5555
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "airflow_sg"
+    Project = "MLOps"
+    Owner   = "Farisology"
+  }
+}
+
+resource "aws_instance" "airflow" {
+  ami           = "ami-0df7a207adb9748c7"
+  instance_type = "t3.medium"
+  key_name      = "zenml_server"
+  root_block_device {
+    volume_size = 30
+  }
+  vpc_security_group_ids = [
+    module.dev_ssh_sg.security_group_id,
+    module.default_ec2_sg.security_group_id,
+    aws_security_group.airflow_sg.id
+  ]
+  tags = {
+    Name    = "Airflow"
+    Project = "MLOps"
+    Owner   = "Farisology"
+  }
+  #   user_data = file("init_airflow.sh")
 }
