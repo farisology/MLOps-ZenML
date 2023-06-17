aa31bec21579858c097e308c903c12098b2bd52c
Fares Hasan <farishassan2012@gmail.com>
2023-06-18 00:32:53 +0800

    remote airflow pipeline run

--- fatty_liver/r_nfld_run.py
+++ fatty_liver/r_nfld_run.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from zenml.config import DockerSettings
 from zenml.config.schedule import Schedule
 from pipelines.nfld_pipeline import training_nfld_model
@@ -7,27 +8,33 @@ from steps.nfld_steps import (import_data, preprocess_data,
                               training_SVC, training_dct
                               )
 
+date_time = datetime.datetime.now()
+
 airflow_settings = AirflowOrchestratorSettings(
     operator="docker",  # or "kubernetes_pod"
-    # dag_output_dir=f"{os.getcwd()}/zipped_pipelines", #use your directory path to the zipped_pipes directory
-    dag_id="non_fatty_liver_training",
+    # use your directory path to the zipped_pipes directory
+    dag_output_dir=f"{os.getcwd()}/zipped_pipelines",
+    dag_id=f"non_fatty_liver_trainingx",
     dag_tags=["zenml", "MLOPS", "Training"],
     # custom_dag_generator="zenml_custom_dag_template"
 )
-docker_settings = DockerSettings(requirements=["pandas", "scikit-learn", "boto3", "botocore==1.29.146"])
+docker_settings = DockerSettings(
+    requirements=["pandas", "scikit-learn", "boto3", "botocore==1.29.146"])
+
 
 def main():
     # init and run the nfdl classifier training pipeline
     schedule = Schedule(cron_expression="5-15 * * * *")
     run_nfdl_training = training_nfld_model(
-        import_data=import_data(),
-        preprocess_data=preprocess_data(),
-        training_SVC=training_SVC(),
-        training_dct=training_dct()
+        import_datax=import_data(),
+        preprocess_datax=preprocess_data(),
+        training_SVCx=training_SVC(),
+        training_dctx=training_dct()
     )
 
     run_nfdl_training.run(settings={"orchestrator.airflow": airflow_settings,
-                                    "docker": docker_settings}, schedule=schedule)
+                                    "docker": docker_settings}, schedule=schedule,
+                          run_name=f"custom_pipeline_run_name_{date_time}")  # this to create unique run name and avoid EntityExistsError
 
 
 if __name__ == "__main__":
